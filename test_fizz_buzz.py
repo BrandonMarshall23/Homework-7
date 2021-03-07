e67ff5c9d86017d4cd59a369e1450e01904552be
BrandonMarshall23 <77769732+BrandonMarshall23@users.noreply.github.com>
2021-03-06 18:53:23 -0800

    Update test_fizz_buzz.py
    THE FINAL TEST!!!!!!!!!!!!!!

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -7,5 +7,8 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz.process(20), 'Buzz')
     def test_fizzbuzz(self):
         self.assertEqual(fizzbuzz.process(15), 'FizzBuzz')
+    def test_regular_numbers(self):
+        self.assertEqual(fizzbuzz.process(2), 2)
+        self.assertEqual(fizzbuzz.process(98), 98)
 if __name__ == '__main__':
     unittest.main()
