4d87a5e86c7ea5258e794ade5c0c09bd21b2f85b
BrandonMarshall23 <77769732+BrandonMarshall23@users.noreply.github.com>
2021-03-06 18:49:54 -0800

    Update test_fizz_buzz.py
    Added fizz buzz test

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -5,5 +5,7 @@ class TestFizzBuzz(unittest.TestCase):
         self.assertEqual(fizzbuzz.process(6), 'Fizz')
     def test_multiple_of_five(self):
         self.assertEqual(fizzbuzz.process(20), 'Buzz')
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz.process(15), 'FizzBuzz')
 if __name__ == '__main__':
     unittest.main()
