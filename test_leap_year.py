7acb9c9e3c09319485899ee9df9bda65cd7c2c35
BrandonMarshall23 <77769732+BrandonMarshall23@users.noreply.github.com>
2021-03-06 19:08:37 -0800

    Update test_leap_year.py
    Divisible by 100 test

--- test_leap_year.py
+++ test_leap_year.py
@@ -3,5 +3,7 @@ import unittest
 class TestLeapYear(unittest.TestCase):
     def div_by_4(self):
        self.assertEqual(leap_year.process, True)
+    def div_by_100(self):
+       self.assertEqual(leap_year.process, True)
 if __name__ == '__main__':
     unittest.main()
